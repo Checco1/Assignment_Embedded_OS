84c5699926339ac9965d18a40a855a09f708a706
Checco1 <s296579@studenti.polito.it>
2023-08-17 20:52:32 +0200

    Configuration file added

---
+++ Router_V0.1/applications/Thread_Function/config.h
@@ -0,0 +1,22 @@
+#ifndef APPLICATIONS_THREAD_FUNCTION_CONFIG_H_
+#define APPLICATIONS_THREAD_FUNCTION_CONFIG_H_
+
+#define MAX_USER 100
+#define MAX_CHAR 256
+
+#define true 1
+#define false 0
+
+typedef struct user {
+    char name[MAX_CHAR];
+    int isActive;
+    //int position;
+    int distance[MAX_USER];
+
+} usr;
+
+usr users[MAX_USER];
+
+int g_matrix[MAX_USER][MAX_USER];
+
+#endif
